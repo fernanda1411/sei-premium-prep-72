2a2ef0074a49a235dae03a430336b0e58b93765d
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 14:56:50 -0800

    Get full name

---
+++ Module1/Part1/getFullName/getFullName.js
@@ -0,0 +1,15 @@
+/*
+Write a function called "getFullName". 
+Given a first and a last name, "getFullName" 
+returns a single string with the given first and 
+last names separated by a single space.
+
+*/
+
+function getFullName(firstName, lastName) {
+    const fullName = (firstName + " " + lastName);
+    return fullName;
+}
+
+var output = getFullName('Joe', 'Smith');
+console.log(output); // --> 'Joe Smith'
