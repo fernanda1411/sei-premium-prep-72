3fcfd99c9684d0c41f477c2d7bc954f35eb93ccd
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 15:23:23 -0800

    Is odd length

---
+++ Module1/Part1/isOddLength/isOddLength.js
@@ -0,0 +1,17 @@
+/*
+Write a function called "isOddLength".
+
+Given a word, "isOddLength" returns whether the length of the given word is odd.
+
+*/
+
+function isOddLength(word) {
+    if(word.length % 2 === 1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var output = isOddLength('special');
+console.log(output); // --> true
