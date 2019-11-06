5be2d7628f73a27bb07437eb03a296e730433ef6
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 15:13:27 -0800

    Is equal to

---
+++ Module1/Part1/isEqualTo/isEqualTo.js
@@ -0,0 +1,15 @@
+/*
+Write a function called "isEqualTo". 
+Given 2 numbers, "isEqualTo" returns whether num2 is equal to num1.
+*/
+
+function isEqualTo(num1, num2) {
+    if(num1 === num2){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var output = isEqualTo(11, 10);
+console.log(output); // --> false
