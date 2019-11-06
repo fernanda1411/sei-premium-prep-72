823d97d55e489e96123057c3ee5524c5b95b1624
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 15:08:08 -0800

    Equals then

---
+++ Module1/Part1/equalsTen/equalsTen.js
@@ -0,0 +1,15 @@
+/*
+Write a function called "equalsTen". 
+Given a number, "equalsTen" returns whether or not the given number is 10.
+*/
+
+function equalsTen(num) {
+    if(num === 10){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var output = equalsTen(9);
+console.log(output); // --> false
