8faf76a52b0356aea322ea6f5ec55c20944cbb46
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 15:21:38 -0800

    Is either even

---
+++ Module1/Part1/isEitherEven/isEitherEven.js
@@ -0,0 +1,18 @@
+/*
+Write a function called "isEitherEven".
+
+Given two numbers, "isEitherEven" 
+returns whether or not either one of the given numbers is even.
+
+*/
+
+function isEitherEven(num1, num2) {
+    if(num1 % 2 !== 1 || num2 % 2 !== 1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var output = isEitherEven(1, 4);
+console.log(output); // --> true
