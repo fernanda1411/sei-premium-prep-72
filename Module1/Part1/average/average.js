531d3b8b16161241ae0e497fdc2e01e557fbdb73
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 15:29:17 -0800

    Average

---
+++ Module1/Part1/average/average.js
@@ -0,0 +1,13 @@
+/*
+Write a function called "average".
+
+Given two numbers, "average" returns their average.
+
+*/
+
+function average(num1, num2) {
+    return (num1 + num2) / 2;
+}
+
+var output = average(4, 6);
+console.log(output); // --> 5
