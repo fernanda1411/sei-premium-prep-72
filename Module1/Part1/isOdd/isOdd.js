f821a46c2acd3fa5930763099f911468c3c8708a
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 15:16:43 -0800

    Is odd

---
+++ Module1/Part1/isOdd/isOdd.js
@@ -0,0 +1,15 @@
+/*
+Write a function called "isOdd". 
+Given a number, "isOdd" returns whether the given number is odd.
+*/
+
+function isOdd(num) {
+    if(num % 2 === 1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var output = isOdd(9);
+console.log(output); // --> true
