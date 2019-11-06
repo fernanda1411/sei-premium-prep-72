0a69c02f71f0d4c7b6a02d317f77d09aa4ad7756
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 15:34:13 -0800

    Square

---
+++ Module1/Part1/square/square.js
@@ -0,0 +1,13 @@
+/*
+Write a function called "square".
+
+Given a number, "square" should return the square of the given number.
+
+*/
+
+function square(num) {
+    return Math.pow(num, 2);
+}
+
+var output = square(5);
+console.log(output); // --> 25
