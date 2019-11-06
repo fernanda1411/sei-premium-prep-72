5fe672d811a4c8449319bb34dc09e8affabd8906
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 15:25:01 -0800

    Is even length

---
+++ Module1/Part1/isEvenLength/isEvenLength.js
@@ -0,0 +1,16 @@
+/*
+Write a function called "isEvenLength".
+
+Given a word, "isEvenLength" returns whether the length of the word is even.
+*/
+
+function isEvenLength(word) {
+    if(word.length % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var output = isEvenLength('wow');
+console.log(output); // --> false
