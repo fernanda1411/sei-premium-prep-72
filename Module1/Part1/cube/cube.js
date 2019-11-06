10d92f4d255b2d617306fc474c6d1c0cc1c7afa7
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 15:32:40 -0800

    Cube

---
+++ Module1/Part1/cube/cube.js
@@ -0,0 +1,13 @@
+/*
+Write a function called "cube".
+
+Given a number, "cube" returns the cube of that number.
+
+*/
+
+function cube(num) {
+    return Math.pow(num, 3);
+}
+
+var output = cube(3);
+console.log(output); // --> 27
