c0ef4f2306dc19ffdb504e7252f5b4d363195e6c
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 15:15:02 -0800

    Is even

---
+++ Module1/Part1/isEven/isEven.js
@@ -0,0 +1,15 @@
+/*
+Write a function called "isEven". 
+Given a number, "isEven" returns whether it is even.
+*/
+
+function isEven(num) {
+    if(num % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var output = isEven(11);
+console.log(output); // --> false
