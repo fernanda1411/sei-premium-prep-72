e89292f31809579a60938a34af1e9728ba3d3ec1
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 16:10:11 -0800

    Compute power

---
+++ Module1/Part2/computePower/computePower.js
@@ -0,0 +1,14 @@
+/*
+Write a function called "computePower".
+
+Given a number and an exponent, "computePower" 
+returns the given number, raised to the given exponent.
+
+*/
+
+function computePower(num, exponent) {
+    return Math.pow(num, exponent);
+}
+
+var output = computePower(2, 3);
+console.log(output); // --> 8
