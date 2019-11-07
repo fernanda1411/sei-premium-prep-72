3e935efa5f272af2a849b41d894aa03c947c435e
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 18:03:08 -0800

    Count number of keys

---
+++ Module1/Part3/countNumberOfKeys/countNumberOfKeys.js
@@ -0,0 +1,22 @@
+/*
+Write a function called "countNumberOfKeys".
+
+Given an object, "countNumberOfKeys" returns 
+how many properties the given object has.
+*/
+
+function countNumberOfKeys(obj) {
+    let count = 0;
+    for(let key in obj){
+        count++;
+    }
+    return count;
+}
+
+var obj = {
+    a: 1,
+    b: 2,
+    c: 3
+  };
+  var output = countNumberOfKeys(obj);
+  console.log(output); // --> 3
