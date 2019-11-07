27ec08acfc236675dcab0aee199385f67bea45a9
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 16:13:31 -0800

    Double square root of

---
+++ Module1/Part2/doubleSquareRootOf/doubleSquareRootOf.js
@@ -0,0 +1,11 @@
+/*
+Write a function called "doubleSquareRootOf". 
+Given a number, "doubleSquareRootOf" returns double its square root.
+*/
+
+function doubleSquareRootOf(num) {
+    return Math.sqrt(num) * 2;
+}
+
+var output = doubleSquareRootOf(121);
+console.log(output); // --> 22
