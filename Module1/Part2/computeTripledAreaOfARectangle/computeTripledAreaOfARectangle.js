b70ecd60f32dc7a4b82643895a49359f8f00ba5c
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 16:04:42 -0800

    Compute tripled area of a rectangle

---
+++ Module1/Part2/computeTripledAreaOfARectangle/computeTripledAreaOfARectangle.js
@@ -0,0 +1,14 @@
+/*
+Write a function called "computeTripledAreaOfARectangle".
+
+Given a length and width of a rectangle, "computeTripledAreaOfARectangle" 
+returns the rectangle's area, multiplied by 3.
+
+*/
+
+function computeTripledAreaOfARectangle(length, width) {
+    return (length * width) * 3;
+}
+
+var output = computeTripledAreaOfARectangle(2, 4);
+console.log(output); // --> 24
