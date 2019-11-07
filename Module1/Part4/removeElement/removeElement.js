82aef57bc33aefca57870c510b8cf9e2812a58dc
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-07 14:42:50 -0800

    Remove element

---
+++ Module1/Part4/removeElement/removeElement.js
@@ -0,0 +1,25 @@
+/*
+Write a function called "removeElement".
+
+Given an array of elements, and a "discarder" parameter, 
+"removeElement" returns an array containing the items in the 
+given array that do not match the "discarder" parameter.
+
+Notes:
+
+If all the elements match, it should return an empty array.
+If an empty array is passed in, it should return an empty array.
+*/
+
+function removeElement(array, discarder) {
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== discarder) {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+}
+
+var output = removeElement([1, 2, 3, 2, 1], 2);
+console.log(output); // --> [1, 3, 1]
