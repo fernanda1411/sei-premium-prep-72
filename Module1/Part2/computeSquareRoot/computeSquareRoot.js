ebcc72a56e73cebb07b8b5912b299455ee199294
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-11-06 16:11:52 -0800

    Compute square root

---
+++ Module1/Part2/computeSquareRoot/computeSquareRoot.js
@@ -0,0 +1,11 @@
+/*
+Write a function called "computeSquareRoot". 
+Given a number, "computeSquareRoot" returns its square root.
+*/
+
+function computeSquareRoot(num) {
+    return Math.sqrt(num);
+}
+
+var output = computeSquareRoot(9);
+console.log(output); // --> 3
