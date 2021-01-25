aae1303a9bdc7984a4ebc9b43eaac1af8098c3b6
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2021-01-25 12:00:10 -0800

    Adding Module0 - JsOverview && Variables

---
+++ Module0/JsOverview/UsingTheConsole/usingTheConsole.js
@@ -0,0 +1,20 @@
+/*Question
+Please carefully note, the title of the challenge is "Using the Console".
+
+While on this page, open up the developer console, and paste these lines of code. Feel free to return to the previous lesson for more details on how to accomplish this task.
+
+var elements = document.getElementsByClassName("title");
+var target = elements[1];
+target.innerHTML = "DISCOVER YOUR INNER HACKER";
+target.style.background = "black";
+target.style.color = "white";
+
+After pasting the code, hit "Enter". Now, carefully examine the page. What changed when you hit "Enter"? */
+
+var elements = document.getElementsByClassName("title");
+var target = elements[1];
+target.innerHTML = "DISCOVER YOUR INNER HACKER";
+target.style.background = "black";
+target.style.color = "white";
+
+// The title of the challenged changed its contents and color
