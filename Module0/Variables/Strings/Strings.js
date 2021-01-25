58eb225d5c2e84f5f2874a7753908e19ea423ac6
FERNANDA SILVA <fernanda.rodrigues.cdc@gmail.com>
2021-01-25 12:25:16 -0800

    Update Strings.js

--- Module0/Variables/Strings/Strings.js
+++ Module0/Variables/Strings/Strings.js
@@ -1,4 +1,5 @@
-/*Lastly, we are going to complete a function that takes two parameters, a string and number (will refer to an index within the string). The function should return the character within the string, located at the given number index. */
+/*Lastly, we are going to complete a function that takes two parameters, a string and number (will refer to an index within the string). 
+The function should return the character within the string, located at the given number index. */
 
 function returnACharacter(string, index) {
     // returns string character at given index
